f797d969e71ce59e43e2f9d1f4dd9484396d184e
Shahab <shahabsafaei@protonmail.com>
2020-02-24 01:03:51 +0330

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -10,13 +10,15 @@ sudo yum -y install python3 python3-pip
 else
 exit
 fi
+
 pip3 install PyQt5
+
 mkdir ~/.openvpn
 mv main.py ~/.openvpn
 mv openvpn.png ~/.openvpn
 read -p 'Enter your openvpn config full path (.ovpn file) ' ovpn
 mv $ovpn ~/.openvpn/vpn.ovpn
-read -p 'Enter your username ' home
+
 echo "[Unit]
 Description=OpenVPN service
 
@@ -24,14 +26,15 @@ Description=OpenVPN service
 Type=simple
 User=root
 Group=root
-WorkingDirectory=/home/$home
-ExecStart=openvpn /home/$home/.openvpn/vpn.ovpn
+WorkingDirectory=$HOME
+ExecStart=openvpn $HOME/.openvpn/vpn.ovpn
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/myovpn.service
+
 echo "[Desktop Entry]
 Name=OpenVPN GUI
-Exec=python3 /home/$home/.openvpn/main.py
+Exec=python3 $HOME/.openvpn/main.py
 Type=Application
-Icon= /home/$home/.openvpn/openvpn.png" | sudo tee /usr/share/applications/openvpngui.desktop
+Icon= $HOME/.openvpn/openvpn.png" | sudo tee /usr/share/applications/openvpngui.desktop
